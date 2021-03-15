c6d8ee91a50c50903ca2d845d21b9654a3cbc68b
sashabely221100 <sashabelyy2012@gmail.com>
2021-03-15 23:57:20 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    return (n < 0) ? Number(String(n * (-1)).split('').reverse().join('')) : Number(String(n).split('').reverse().join(''));
+
 }
